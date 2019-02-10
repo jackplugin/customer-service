e4441e063cfb916a4024a72c3a7d64bfbb76a3a2
jack.li.cn <jack.li.cn@outlook.com>
2019-02-10 22:14:04 +0800

    Initial import

---
+++ src/main/java/org/jack/k8s/customerservice/DemoApplication.java
@@ -0,0 +1,14 @@
+package org.jack.k8s.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
